d2c5a527cdd8bda8ae2ecf8c1ac484ca996d55ee
Kaushik7984 <kaushik7984@gmail.com>
2024-12-19 20:24:01 +0530

    some changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,26 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
+  let dbURI;
+  
+  if (process.env.NODE_ENV === "development") {
+    dbURI = process.env.DB_URI_D; // Use local MongoDB URI for development
+  } else {
+    dbURI = process.env.DB_URI_P; // Use MongoDB URI from environment variables for production
+  }
+
+  console.log(`Environment: ${process.env.NODE_ENV}`);
+  console.log(`DB_URI: ${dbURI}`);
+
   mongoose
-    .connect(process.env.DB_URI)
+    .connect(dbURI)
     .then((data) => {
-      console.log(`Mongodb connected with server: ${data.connection.host}`);
+      console.log(`MongoDB connected with server: ${data.connection.host}`);
     })
-    
+    .catch((error) => {
+      console.error("Database connection error:", error.message);
+      process.exit(1); // Exit the process with failure
+    });
 };
 
 module.exports = connectDatabase;
