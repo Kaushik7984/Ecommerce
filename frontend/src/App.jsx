85c406135ef2cf1d31123d3c5644bfaab8d1b883
Kaushik7984 <kaushik7984@gmail.com>
2024-12-22 15:29:14 +0530

    add lazy loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";  // Import Suspense for lazy loading
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -30,18 +30,22 @@ import Payment from "./components/Cart/Payment.jsx";
 import OrderSuccess from "./components/Cart/OrderSuccess.jsx";
 import MyOrders from "./components/Order/MyOrders.jsx";
 import OrderDetails from "./components/Order/OrderDetails.jsx";
-import Dashboard from "./components/Admin/Dashboard";
-import ProductList from "./components/Admin/ProductList.jsx";
-import NewProduct from "./components/Admin/NewProduct.jsx";
-import UpdateProduct from "./components/Admin/UpdateProduct.jsx";
-import OrderList from "./components/Admin/OrderList.jsx";
-import ProcessOrder from "./components/Admin/ProcessOrder.jsx";
-import UsersList from "./components/Admin/UsersList.jsx";
-import UpdateUser from "./components/Admin/UpdateUser.jsx";
-import ProductReviews from "./components/Admin/ProductReviews.jsx";
+
+// Use lazy loading for Dashboard and other admin components
+const Dashboard = React.lazy(() => import("./components/Admin/Dashboard"));
+const ProductList = React.lazy(() => import("./components/Admin/ProductList.jsx"));
+const NewProduct = React.lazy(() => import("./components/Admin/NewProduct.jsx"));
+const UpdateProduct = React.lazy(() => import("./components/Admin/UpdateProduct.jsx"));
+const OrderList = React.lazy(() => import("./components/Admin/OrderList.jsx"));
+const ProcessOrder = React.lazy(() => import("./components/Admin/ProcessOrder.jsx"));
+const UsersList = React.lazy(() => import("./components/Admin/UsersList.jsx"));
+const UpdateUser = React.lazy(() => import("./components/Admin/UpdateUser.jsx"));
+const ProductReviews = React.lazy(() => import("./components/Admin/ProductReviews.jsx"));
+
 import About from "./components/layout/About/About.jsx";
 import Contact from "./components/layout/Contact/Contact.jsx";
 import NotFound from "./components/layout/Not Found/NotFound.jsx";
+import Loader from "./components/layout/Loader/Loader.jsx";
 
 function App() {
   const { isAuthenticated, user } = useSelector((state) => state.user);
@@ -69,65 +73,64 @@ function App() {
     getStripeApiKey();
   }, []);
 
-  // if (loading) {
-  //   return null; 
-  // }
-
   return (
     <Router>
       <Header />
       {isAuthenticated && <UserOptions user={user} />}
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:keyword" element={<Products />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/login" element={<LoginSignUp />} />
-        <Route path="/password/forgot" element={<ForgotPassword />} />
-        <Route path="/password/reset/:token" element={<ResetPassword />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
+      <Suspense fallback={<Loader />}> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:keyword" element={<Products />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/login" element={<LoginSignUp />} />
+          <Route path="/password/forgot" element={<ForgotPassword />} />
+          <Route path="/password/reset/:token" element={<ResetPassword />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
 
-        {/* Protected Routes */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/account" element={<Profile />} />
-          <Route path="/me/update" element={<UpdateProfile />} />
-          <Route path="/password/update" element={<UpdatePassword />} />
-          <Route path="/shipping" element={<Shipping />} />
-          <Route path="/order/confirm" element={<ConfirmOrder />} />
-          {stripeApiKey && (
-            <Route
-              path="/process/payment"
-              element={
-                <Elements stripe={loadStripe(stripeApiKey)}>
-                  <Payment />
-                </Elements>
-              }
-            />
-          )}
-          <Route path="/success" element={<OrderSuccess />} />
-          <Route path="/orders" element={<MyOrders />} />
-          <Route path="/order/:id" element={<OrderDetails />} />
-        </Route>
+          {/* Protected Routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/account" element={<Profile />} />
+            <Route path="/me/update" element={<UpdateProfile />} />
+            <Route path="/password/update" element={<UpdatePassword />} />
+            <Route path="/shipping" element={<Shipping />} />
+            <Route path="/order/confirm" element={<ConfirmOrder />} />
+            {stripeApiKey && (
+              <Route
+                path="/process/payment"
+                element={
+                  <Elements stripe={loadStripe(stripeApiKey)}>
+                    <Payment />
+                  </Elements>
+                }
+              />
+            )}
+            <Route path="/success" element={<OrderSuccess />} />
+            <Route path="/orders" element={<MyOrders />} />
+            <Route path="/order/:id" element={<OrderDetails />} />
+          </Route>
 
-        {/* Admin Protected Route */}
-        <Route element={<ProtectedRoute isAdmin={true} />}>
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/products" element={<ProductList />} />
-          <Route path="/admin/product" element={<NewProduct />} />
-          <Route path="/admin/product/:id" element={<UpdateProduct />} />
-          <Route path="/admin/orders" element={<OrderList />} />
-          <Route path="/admin/order/:id" element={<ProcessOrder />} />
-          <Route path="/admin/users" element={<UsersList />} />
-          <Route path="/admin/user/:id" element={<UpdateUser />} />
-          <Route path="/admin/reviews" element={<ProductReviews />} />
-        </Route>
+          {/* Admin Protected Route */}
+          <Route element={<ProtectedRoute isAdmin={true} />}>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/products" element={<ProductList />} />
+            <Route path="/admin/product" element={<NewProduct />} />
+            <Route path="/admin/product/:id" element={<UpdateProduct />} />
+            <Route path="/admin/orders" element={<OrderList />} />
+            <Route path="/admin/order/:id" element={<ProcessOrder />} />
+            <Route path="/admin/users" element={<UsersList />} />
+            <Route path="/admin/user/:id" element={<UpdateUser />} />
+            <Route path="/admin/reviews" element={<ProductReviews />} />
+          </Route>
 
-      </Routes>
+        </Routes>
+      </Suspense>
+
       <Footer />
     </Router>
   );
