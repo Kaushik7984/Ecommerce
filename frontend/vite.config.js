422afb657d1d2c645e8bfa79af5b72c5e0f1e121
Kaushik7984 <kaushik7984@gmail.com>
2024-12-22 15:48:31 +0530

    some change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Only for local development
-      '/api': {
-        target: 'http://localhost:4000',  
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), 
-      },
-    },
-  },
-  build: {
-    outDir: "dist", 
-  },
-});
+})
