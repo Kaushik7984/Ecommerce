c63be97b6f750d370ba217a55c6462de41a0b6d1
Kaushik7984 <kaushik7984@gmail.com>
2024-12-24 15:29:49 +0530

    change in frontend

--- frontend/src/components/url.js
+++ frontend/src/components/url.js
@@ -1,4 +1,4 @@
-// const backend_url = "http://localhost:4000";  //for Development mode 
-const backend_url = "https://ecommercebackend-s73c.onrender.com";  //for Production mode 
+const backend_url = "http://localhost:4000";  //for Development mode 
+// const backend_url = "https://ecommercebackend-s73c.onrender.com";  //for Production mode 
 
 export default backend_url;
